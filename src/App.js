082000a541019c0296a3d8cfec38db115259650b
abelmaro <contactomaronas@gmail.com>
2022-09-19 01:11:47 -0300

    Landing page layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { motion } from "framer-motion";
+import Navbar from "./components/Navbar";
+import InfoItem from "./components/InfoItem";
+import Footer from "./components/Footer";
+import ContactForm from "./components/ContactForm";
+import { cardsItems } from "./constants/cardsItems";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <>
+            <header className="max-w-screen-lg m-auto sticky top-0 z-50">
+                <Navbar />
+            </header>
+            <main
+                id="home-container"
+                className="relative max-w-screen-lg m-auto"
+            >
+                <div className="flex justify-center max-w-screen-xl">
+                    <div className="flex justify-center">
+                        <div className="w-8/12 flex items-center text-center flex-col absolute top-10 z-10 text-[white]">
+                            <motion.h1
+                                initial={{ y: -150 }}
+                                whileInView={{ y: 0 }}
+                                transition={{ duration: 0.6 }}
+                                className="text-7xl"
+                            >
+                                CERALIST
+                            </motion.h1>
+                            <motion.span
+                                initial={{ y: -200 }}
+                                whileInView={{ y: 0 }}
+                                transition={{ duration: 0.6, delay: 0.1 }}
+                                className="text-2xl lg:block hidden"
+                            >
+                                The character of the space that surrounds us is
+                                so powerful that it can cause changes in what we
+                                are or think we are.
+                            </motion.span>
+                        </div>
+                        <img
+                            src="./assets/images/ceralist-header.jpg"
+                            alt="Ceralist header"
+                            className="relative"
+                        />
+                    </div>
+                </div>
+                <div id="products">
+                    {cardsItems.map((card, index) => {
+                        return (
+                            <InfoItem
+                                imgLeft={card.imgLeft}
+                                title={card.title}
+                                details={card.details}
+                                imgPath={card.imgPath}
+                                imgAlt={card.imgAlt}
+                                key={"mug-card-" + index}
+                                index={index}
+                            />
+                        );
+                    })}
+                </div>
+                <div className="w-full px-12 pt-10 flex flex-col align-center items-center justify-center lg:justify-between">
+                    <span className="text-3xl lg:text-5xl">Our history</span>
+                    <motion.img
+                        className="rounded-[20px] w-60 lg:w-8/12 my-10"
+                        src="./assets/images/ceralist-bottles.jpg"
+                        alt="Ceralist cooking pot"
+                        animate={{opacity:0}}
+                        whileInView={{opacity:1}}
+                        transition={{duration: 1}}
+                    />
+                    <p className="text-base">
+                        Ceralist is a personal project that arises from my need
+                        to re-create and model with my hands. Keeping the mind
+                        entertained in the activity becomes something healing
+                        and relaxing at the same time... it is a therapy
+                    </p>
+                </div>
+                <ContactForm />
+            </main>
+            <Footer />
+        </>
+    );
 }
 
 export default App;
